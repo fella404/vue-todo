016c20bcf3f9b9e6ea81788a907222202f765db9
murdock33 <tomhagen849@gmail.com>
2025-03-04 10:35:23 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  data() {
+    return {
+      todoInput: "",
+      idx: 0,
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.todoInput !== "") {
+        this.todos.push({
+          id: this.idx++,
+          act: this.todoInput,
+          completed: false,
+        });
+        this.todoInput = "";
+      }
+    },
+    deleteTodo(todo) {
+      this.todos = this.todos.filter((t) => t.id !== todo.id);
+    },
+    isCompleted(t) {
+      for (const todo of this.todos) {
+        if (todo.id === t.id) {
+          todo.completed = !todo.completed;
+        }
+      }
+      console.log(this.todos);
+    },
+    enterTodo() {
+      this.addTodo();
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <section class="input-group my-3">
+      <input
+        type="text"
+        class="form-control"
+        v-model="todoInput"
+        @keydown.enter="enterTodo"
+      />
+      <button @click="addTodo" class="btn btn-outline-secondary">Add</button>
+    </section>
+    <ul class="list-group" v-for="todo in todos" :key="todo.id">
+      <li
+        class="d-flex justify-content-between align-items-center list-group-item"
+      >
+        <p class="m-0" :class="{ completed: todo.completed }">{{ todo.act }}</p>
+        <div class="btn-group">
+          <button class="btn btn-danger me-1" @click="deleteTodo(todo)">
+            Delete
+          </button>
+          <button class="btn btn-success" @click="isCompleted(todo)">
+            {{ todo.completed ? "&#10005;" : "&#10003;" }}
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.completed {
+  text-decoration: line-through;
+}
+</style>
